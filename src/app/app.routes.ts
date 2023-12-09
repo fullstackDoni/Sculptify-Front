4326ba7c71c8c047a6d2ef03b57139cc41681579
unknown <daniyar.doskulov@narxoz.kz>
2023-12-10 02:51:18 +0600

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import {Routes, RouterModule} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommentComponent} from './comment/comment.component';
+import {ProfileComponent} from './profile/profile.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {ArticleComponent} from './article/article.component'
+import {AboutusComponent} from "./aboutus/aboutus.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/blog', pathMatch: 'full'},
+  {path: 'comment', component: CommentComponent},
+  {path: 'article', component: ArticleComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'about-us', component: AboutusComponent},
+  {path: '**', redirectTo: '/blog'},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {
+}
